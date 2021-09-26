6eb47a461b914da22c754f2516b21d34e333d444
RamadhaniRiparro <ramadhani9112@gmail.com>
2021-09-26 14:21:41 +0700

    new solutions first commit

---
+++ JavaScript Algorithms and Data Structures/01-Basic Javascript/01. Comment Your JavaScript Code/answer.js
@@ -0,0 +1,3 @@
+// This is an in-line comment in javascript langauge
+/* This is a
+multi-line comment in javascript language*/
---
+++ JavaScript Algorithms and Data Structures/01-Basic Javascript/17. Finding a Remainder in JavaScriptPassed/answer.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+remainder = 5 % 3;
+var remainder;
